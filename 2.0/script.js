28aec866e8666e4cbaceb234794b1ee5f3a8d0fe
Colt <98503459+coltschultz@users.noreply.github.com>
2022-03-24 14:14:02 -0500

    more changes

---
+++ 2.0/script.js
@@ -0,0 +1,27 @@
+const work = [ssmga, ];
+const ssmga = {
+    url: 'https://www.southernstarmga.com',
+    git: 'N/A'
+    name: 'Southern Star MGA',
+    desc: 'A landing page designed for an insurance managing general agency that I maintain as a part of my current job duties.',
+    photo: './assets/img/ssmga.jpg'
+    };
+cosnt licona = {
+    url: 'https://www.liconainsurance.com',
+    git: 'N/A',
+    name: 'Licona Insurance Group',
+    desc: 'A landing page designed for a retail insurance agency that I maintain as a part of my current job duties.',
+    photo: './assets/img/licona.jpg'
+    };
+
+const contentEl = document.querySelector('#contentContainer');
+const viewLinkEl2 = document.querySelector('#viewLink2');
+
+const clearContent => {
+    contentEl.innerHTML = '';
+};
+
+const showWork => {
+    clearContent();
+
+};
